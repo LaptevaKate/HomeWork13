18a347c3d67f6def046226070e463668bea6b858
LaptevaKate <ekaterina213@yandex.ru>
2022-05-02 21:59:22 +0300

    load image from url

--- HomeWork13/Controller/MainViewController.swift
+++ HomeWork13/Controller/MainViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 class MainViewController: UIViewController {
     
@@ -14,7 +15,7 @@ class MainViewController: UIViewController {
     
     //MARK: - Properties
     var imagesArray = ImageSavingSettings.getAllImages()
-    
+
     //MARK: - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +24,20 @@ class MainViewController: UIViewController {
         navigationController?.setNavigationBarHidden(true, animated: false)
     }
     
+    //MARK: - Methods
+    func downloadImage(url: URL) {
+        let downloader = ImageDownloader.default
+        downloader.downloadImage(with: url) { result in
+            switch result {
+            case .success(let value):
+                self.presentAlert(text: "Your image in Gallery")
+                UIImageWriteToSavedPhotosAlbum(value.image, nil, nil, nil)
+            case .failure(let error):
+                self.presentAlert(text: "Check the kind of url")
+            }
+        }
+    }
+
     //MARK: - IBActions
     @IBAction func plusImageButtonTapped(_ sender: Any) {
         let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
@@ -31,11 +46,23 @@ class MainViewController: UIViewController {
                                       handler: { [weak self] _ in
             self?.openGallery()
         }))
+        alert.addAction(UIAlertAction(title: "Camera",
+                                      style: .default,
+                                      handler: { [weak self] _ in
+            self?.openCamera()
+        }))
+        alert.addAction(UIAlertAction(title: "Load from Link to Gallery",
+                                      style: .default,
+                                      handler: { [weak self] _ in
+            self?.openLink()
+        }))
+        
         alert.addAction(UIAlertAction(title: "Cancel",
                                       style: .cancel))
         present(alert, animated: true)
     }
 }
+
 //MARK: - UICollectionViewDelegate, UICollectionViewDataSource
 extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -48,6 +75,7 @@ extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSour
         return cell
     }
 }
+
 //MARK: - UICollectionViewDelegateFlowLayout
 extension MainViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -55,6 +83,7 @@ extension MainViewController: UICollectionViewDelegateFlowLayout {
         let addindSpace = 20 * (rowItem + 1)
         let possibleWidth = collectionView.frame.width - addindSpace
         let size = possibleWidth / rowItem
+        
         return CGSize(width: size, height: size)
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
@@ -62,28 +91,15 @@ extension MainViewController: UICollectionViewDelegateFlowLayout {
         return sectionsInserts
     }
 }
+
 extension MainViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let pickedImage = info[.originalImage] as? UIImage {
             imagesArray += [pickedImage]
             ImageSavingSettings.add(pickedImage)
             collectionView.reloadData()
-    }
+        }
         picker.dismiss(animated: true, completion: nil)
     }
 }
-private extension MainViewController {
-    func openGallery() {
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary) {
-            let imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.allowsEditing = true
-            imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
-            self.present(imagePicker, animated: true, completion: nil)
-        } else {
-            let alert = UIAlertController(title: "Warning", message: "No permission", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-        }
-    }
-}
+
